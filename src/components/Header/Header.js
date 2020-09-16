96ed0a9c8dbd9bf78a03f2e563ee269b8bb94215
codersumon <mdsumon.rkb@gmail.com>
2020-09-16 18:48:32 +0600

    ema jhon simple updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <img src={logo} alt="" />
+      <img src={logo} alt=""></img>
       <nav>
         <a href="/shop">Shop</a>
-        <a href="/review">Order Review</a>
-        <a href="/manage">Manage Inventory</a>
+        <a href="/review">Order Review </a>
+        <a href="/manage">Manage Inventory </a>
       </nav>
     </div>
   );
